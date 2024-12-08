769dadfc392d44fa7583aefce352ca208f87298b
healcaraz <healcaraz@gmail.com>
2024-12-08 00:04:19 +0000

    add portfolio components

---
+++ src/helpers/experienceHelper.js
@@ -0,0 +1,20 @@
+export const EXPERIENCE = [
+  {
+    date: '',
+    title: '',
+    description: '',
+    link: '#'
+  },
+  {
+    date: '',
+    title: '',
+    description: '',
+    link: '#'
+  },
+  {
+    date: '',
+    title: '',
+    description: '',
+    link: '#'
+  }
+];
