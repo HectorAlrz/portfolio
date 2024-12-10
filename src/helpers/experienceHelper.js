863603f22ed4df819d1f9b2c227275ed98c1b31e
healcaraz <healcaraz@gmail.com>
2024-12-10 01:58:35 +0000

    add: styles for responsive

--- src/helpers/experienceHelper.js
+++ src/helpers/experienceHelper.js
@@ -1,20 +1,21 @@
 export const EXPERIENCE = [
   {
-    date: '',
-    title: '',
-    description: '',
-    link: '#'
+    id: 1,
+    date: 'January 2022 - October 2024',
+    title: 'Front-End Developer Jr',
+    description: `Contractor in Initialized Capital, transitioned a legacy JavaScript and Flow codebase to TypeScript, enhancing codebase maintainability, making it easier to onboard new developers, and improving the developer experience.
+    Set up comprehensive unit testing with Jest and React Testing Library, alongside Cypress for end-to-end tests, leading to more robust code, earlier bug detection, and increased confidence in future deployments.
+    Migrated the UI from Bootstrap to Material UI, delivering a more modern and responsive user experience while streamlining development workflows through reusable components.
+    Introduced clean code practices via ESLint and enforced consistent code quality standards, resulting in cleaner, more maintainable, and scalable code that is easier to review and collaborate on.
+    Collaborated closely with an international team, implementing new features and ensuring seamless integration of Figma designs into code, while actively resolving bugs and enhancing feature stability.
+    Migration from redux-form to react-final-form for state management, resulting in a more flexible and performant form handling solution, with simplified and more maintainable code.`,
+    link: 'Your project link or company website'
   },
   {
-    date: '',
-    title: '',
-    description: '',
-    link: '#'
-  },
-  {
-    date: '',
-    title: '',
-    description: '',
+    id: 2,
+    date: 'Test',
+    title: 'Lorem ipsum',
+    description: 'Description here',
     link: '#'
   }
 ];
