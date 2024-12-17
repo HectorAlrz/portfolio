5f7e200782736d622f9e8b14cfcc079d8bb15d0e
healcaraz <healcaraz@gmail.com>
2024-12-17 03:03:06 +0000

    add icons and experience update

--- src/helpers/experienceHelper.js
+++ src/helpers/experienceHelper.js
@@ -1,43 +1,45 @@
 export const EXPERIENCE = [
   {
     id: 1,
-    date: 'January 2022 - October 2024',
-    title: 'Front-End Developer',
-    description: `Contractor in Initialized Capital, transitioned a legacy JavaScript and Flow codebase to TypeScript, enhancing codebase maintainability, making it easier to onboard new developers, and improving the developer experience.
-    Set up comprehensive unit testing with Jest and React Testing Library, alongside Cypress for end-to-end tests, leading to more robust code, earlier bug detection, and increased confidence in future deployments.
-    Migrated the UI from Bootstrap to Material UI, delivering a more modern and responsive user experience while streamlining development workflows through reusable components.
-    Introduced clean code practices via ESLint and enforced consistent code quality standards, resulting in cleaner, more maintainable, and scalable code that is easier to review and collaborate on.
-    Collaborated closely with an international team, implementing new features and ensuring seamless integration of Figma designs into code, while actively resolving bugs and enhancing feature stability.
-    Migration from redux-form to react-final-form for state management, resulting in a more flexible and performant form handling solution, with simplified and more maintainable code.`,
-    link: '#'
+    date: "January 2022 - October 2024",
+    title: "Front-End Developer",
+    description: `Contractor at Initialized Capital. Contributed to the transition from a legacy JavaScript and Flow codebase to TypeScript, which improved codebase maintainability. 
+      Supported the implementation of unit testing with Jest and React Testing Library and end-to-end testing with Cypress, ensuring the stability of these migrations and deployment confidence.
+      Assisted in the migration of the UI from Bootstrap to Material UI, focusing on updating components to ensure a modern, responsive design and streamlined development processes
+      Engaged in maintaining code quality by fixing ESLint errors and making minor edits to configurations, which helped maintain clean, scalable, and reviewable code.
+       Collaborated with an international team to integrate Figma designs into functional components, bug fixes, and feature enhancements, ensuring seamless user experience.
+       Participated in updating state management from redux-form to react-final-form, aiding in creating a more flexible and maintainable form handling system.`,
+    link: "#",
   },
   {
     id: 2,
-    date: 'Test',
-    title: 'Lorem ipsum',
-    description: 'Description here',
-    link: '#'
-  }
+    date: "January 2019 - May 2020",
+    title: "Field Engineer",
+    description: "Technical support, maintenance, and commissioning of solar inverters in various solar power plants across Florida, Mexico, and the Dominican Republic",
+    link: "#",
+  },
 ];
 
 export const ES_EXPERIENCE = [
   {
     id: 1,
-    date: 'Enero 2022 - Octubre 2024',
-    title: 'Front-End Developer',
-    description: `Migración de una base de código legado de JavaScript y Flow a TypeScript, mejorando la mantenibilidad del código, facilitando la incorporación de nuevos desarrolladores y optimizando la experiencia del desarrollador.
-    Configuración de pruebas unitarias con Jest y React Testing Library, junto con pruebas de extremo a extremo usando Cypress, lo que permitió detectar errores más temprano y aumentar la confianza en los despliegues.
-    Migración del sistema de diseño de Bootstrap a Material UI, logrando una experiencia de usuario más moderna y responsiva, además de optimizar flujos de trabajo mediante componentes reutilizables.
-    Implementación de estándares de código limpio con ESLint para mantener una calidad de código consistente, obteniendo un código más mantenible, escalable y fácil de colaborar.
-    Colaboración con un equipo internacional para implementar nuevas funcionalidades, integrar diseños de Figma en el código y resolver errores que mejoraron la estabilidad de las características.
-    Migración de redux-form a react-final-form para el manejo de formularios, logrando una solución más flexible y de mejor rendimiento con un código más sencillo de mantener.`,
-    link: '#'
+    date: "Enero 2022 - Octubre 2024",
+    title: "Front-End Developer",
+    description: `Contractor at Initialized Capital. Contribuí a la transición de una base de código antigua en JavaScript y Flow a TypeScript, lo que mejoró la mantenibilidad de la base de código.
+    Apoyé la implementación de pruebas unitarias con Jest y React Testing Library, así como pruebas de extremo a extremo con Cypress, garantizando la estabilidad de estas migraciones y la confianza en el despliegue.
+    Ayudé en la migración de la interfaz de usuario de Bootstrap a Material UI, centrándome en actualizar los componentes para asegurar un diseño moderno, responsivo y procesos de desarrollo más ágiles.
+    Me involucré en el mantenimiento de la calidad del código al corregir errores de ESLint y realizar pequeñas ediciones en las configuraciones, lo que ayudó a mantener el código limpio, escalable y revisable.
+    Colaboré con un equipo internacional para integrar los diseños de Figma en componentes funcionales, corregir errores y mejorar características, garantizando una experiencia de usuario sin interrupciones.
+    Participé en la actualización de la gestión del estado, pasando de redux-form a react-final-form, contribuyendo a la creación de un sistema de manejo de forms más flexible y mantenible.`,
+    link: "#",
   },
   {
     id: 2,
-    date: 'Test',
-    title: 'Lorem ipsum',
-    description: 'Description here',
-    link: '#'
-  }
+    date: "Enero 2019 - Mayo 2020",
+    title: "Ingeniero de Campo",
+    description: "Description here",
+    link: "#",
+  },
 ];
+
+
